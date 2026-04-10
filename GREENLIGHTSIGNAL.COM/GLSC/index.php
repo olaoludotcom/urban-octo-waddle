85f0e663e52dc74a13ad0c82ce1f5c4bda79a879
olaoludotcom <sofatoye@sofatoye.com>
2026-04-10 12:48:37 +0100

    Add files via upload

---
+++ GREENLIGHTSIGNAL.COM/GLSC/index.php
@@ -0,0 +1,181 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Green Light Signal Concept Limited</title>
+<style type="text/css">
+<!--
+body,td,th {
+	font-family: Verdana, Geneva, sans-serif;
+	font-size: 11px;
+	text-align: left;
+}
+body {
+	margin-left: 0px;
+	margin-top: 0px;
+	margin-right: 0px;
+	margin-bottom: 0px;
+}
+c {
+	text-align: justify;
+}
+body p {
+	text-align: justify;
+}
+a:link {
+	color: #060;
+	text-decoration: none;
+}
+a:hover {
+	color: #333;
+	text-decoration: underline;
+}
+a:visited {
+	text-decoration: none;
+	color: #333;
+}
+a:active {
+	text-decoration: none;
+	color: #060;
+}
+.c {
+	text-align: center;
+}
+.c div {
+	text-align: center;
+}
+.c {
+	text-align: center;
+}
+-->
+</style></head>
+
+<body>
+<table width="200" border="0" align="center" cellpadding="0" cellspacing="0">
+  <tr>
+    <td valign="top"><table border="0" cellpadding="0" cellspacing="0" width="1257">
+      <!-- fwtable fwsrc="banner15.png" fwbase="glsc.jpg" fwstyle="Dreamweaver" fwdocid = "1111207420" fwnested="0" -->
+      <tr>
+        <td><img src="images/spacer.gif" width="208" height="1" border="0" alt="" /></td>
+        <td><img src="images/spacer.gif" width="51" height="1" border="0" alt="" /></td>
+        <td><img src="images/spacer.gif" width="209" height="1" border="0" alt="" /></td>
+        <td><img src="images/spacer.gif" width="30" height="1" border="0" alt="" /></td>
+        <td><img src="images/spacer.gif" width="258" height="1" border="0" alt="" /></td>
+        <td><img src="images/spacer.gif" width="34" height="1" border="0" alt="" /></td>
+        <td><img src="images/spacer.gif" width="200" height="1" border="0" alt="" /></td>
+        <td><img src="images/spacer.gif" width="58" height="1" border="0" alt="" /></td>
+        <td><img src="images/spacer.gif" width="209" height="1" border="0" alt="" /></td>
+        <td><img src="images/spacer.gif" width="1" height="1" border="0" alt="" /></td>
+      </tr>
+      <tr>
+        <td colspan="9"><img name="glsc_r1_c1" src="images/glsc_r1_c1.jpg" width="1257" height="208" border="0" id="glsc_r1_c1" alt="" /></td>
+        <td><img src="images/spacer.gif" width="1" height="208" border="0" alt="" /></td>
+      </tr>
+      <tr>
+        <td rowspan="8"><img name="glsc_r2_c1" src="images/glsc_r2_c1.jpg" width="208" height="632" border="0" id="glsc_r2_c1" alt="" /></td>
+        <td colspan="7"><img src="images/glsc_r2_c2.jpg" alt="" name="glsc_r2_c2" width="840" height="26" border="0" usemap="#glsc_r2_c2Map" id="glsc_r2_c2" /></td>
+        <td rowspan="8"><img name="glsc_r2_c9" src="images/glsc_r2_c9.jpg" width="209" height="632" border="0" id="glsc_r2_c9" alt="" /></td>
+        <td><img src="images/spacer.gif" width="1" height="26" border="0" alt="" /></td>
+      </tr>
+      <tr>
+        <td colspan="7"><img name="glsc_r3_c2" src="images/glsc_r3_c2.jpg" width="840" height="26" border="0" id="glsc_r3_c2" alt="" /></td>
+        <td><img src="images/spacer.gif" width="1" height="26" border="0" alt="" /></td>
+      </tr>
+      <tr>
+        <td colspan="7"><img name="glsc_r4_c2" src="images/glsc_r4_c2.jpg" width="840" height="190" border="0" id="glsc_r4_c2" alt="" /></td>
+        <td><img src="images/spacer.gif" width="1" height="190" border="0" alt="" /></td>
+      </tr>
+      <tr>
+        <td rowspan="2"><img name="glsc_r5_c2" src="images/glsc_r5_c2.jpg" width="51" height="58" border="0" id="glsc_r5_c2" alt="" /></td>
+        <td colspan="5"><img name="glsc_r5_c3" src="images/glsc_r5_c3.jpg" width="731" height="28" border="0" id="glsc_r5_c3" alt="" /></td>
+        <td rowspan="2"><img name="glsc_r5_c8" src="images/glsc_r5_c8.jpg" width="58" height="58" border="0" id="glsc_r5_c8" alt="" /></td>
+        <td><img src="images/spacer.gif" width="1" height="28" border="0" alt="" /></td>
+      </tr>
+      <tr>
+        <td colspan="5"><img name="glsc_r6_c3" src="images/glsc_r6_c3.jpg" width="731" height="30" border="0" id="glsc_r6_c3" alt="" /></td>
+        <td><img src="images/spacer.gif" width="1" height="30" border="0" alt="" /></td>
+      </tr>
+      <tr>
+        <td colspan="2"><img name="glsc_r7_c2" src="images/glsc_r7_c2.jpg" width="260" height="45" border="0" id="glsc_r7_c2" alt="" /></td>
+        <td rowspan="2"><img name="glsc_r7_c4" src="images/glsc_r7_c4.jpg" width="30" height="264" border="0" id="glsc_r7_c4" alt="" /></td>
+        <td><img name="glsc_r7_c5" src="images/glsc_r7_c5.jpg" width="258" height="45" border="0" id="glsc_r7_c5" alt="" /></td>
+        <td rowspan="2"><img name="glsc_r7_c6" src="images/glsc_r7_c6.jpg" width="34" height="264" border="0" id="glsc_r7_c6" alt="" /></td>
+        <td colspan="2"><img name="glsc_r7_c7" src="images/glsc_r7_c7.jpg" width="258" height="45" border="0" id="glsc_r7_c7" alt="" /></td>
+        <td><img src="images/spacer.gif" width="1" height="45" border="0" alt="" /></td>
+      </tr>
+      <tr>
+        <td colspan="2" valign="top" background="images/glsc_r8_c2.jpg"><table width="233" height="111" border="0" align="center" cellpadding="0" cellspacing="0">
+          <tr>
+            <td width="233" valign="top"><div>
+              <p>Green Signal Concepts Limited (“GSCL”) is a services management company. The company designs, develops and manages service solutions for optimum utilization of public and private facilities and resources.<br />
+                <br />
+                GSCL activities cover the entire lifecycle of services management with special focus on public resource management for efficiency, economy and effectiveness.</p>
+              <p>...<a href="aboutus.html">Learn more</a></p>
+            </div>
+              <br />
+              <div></div></td>
+          </tr>
+        </table></td>
+        <td valign="top" background="images/glsc_r8_c5.jpg"><table width="233" height="111" border="0" align="center" cellpadding="0" cellspacing="0">
+          <tr>
+            <td width="233" valign="top"><div>
+              <div>
+                <div>GSCL helps private and public sector organizations to:
+                  <br />
+                  <br />
+                </div>
+                <div>&#13;</div>
+                <div>Institute value for money management hinged on resource optimisation and transaction transparency.&#13;<br />
+                  <br />
+                </div>
+                <div>Control their development processes.&#13;</div>
+                <div><br />
+                  Improve and manage quality.&#13;<br />
+                  <br />
+                </div>
+                <div>Prevent waste arising from excessive development and maintenance costs. &#13;<br />
+                </div>
+</div>
+              <br>...<a href="aboutus.html">Learn more</a>
+          
+             
+              </td>
+          </tr>
+        </table></td>
+        <td colspan="2" valign="top" background="images/glsc_r8_c7.jpg">     <marquee onmouseover="this.stop()" onmouseout="this.start()" truespeed="trueSpeed" scrollamount="1" scrolldelay="25" direction="up" width="250" height="200">
+        <table width="220" height="200" border="0" cellpadding="0" cellspacing="0" align="center">
+          <tr>
+            <td width="184" height="195" valign="top"><p align="justify">GreenSignal launches new world class website.  <br />
+              <span class="style5">-----------------------------------------</span></p>
+              <p align="justify">GreenSignal proposed to set-up the Oyo State Trafic Management Authority (OSTMA)<br />
+                <span class="style5">------------------------------------------</span><br />
+                <br />
+                OSTMA to be launch soon to reduce to congested trafic expirence in the cities and towns.<br />
+                <span class="style5">----------------------------------------</span>---</p></td>
+          </tr>
+        </table>
+        </marquee>
+        </td>
+        <td><img src="images/spacer.gif" width="1" height="219" border="0" alt="" /></td>
+      </tr>
+      <tr>
+        <td colspan="7" background="images/glsc_r9_c2.jpg" align="center">
+          <p class="c">Home | About Us | Our Services |  News/Events | Contact | FAQ</p>
+          <p class="c">Copyright &copy; GreenSignal Concepts Limited 2010. All Rights Resevered. | Site Design by Webthinkers </p>
+        </td>
+        <td><img src="images/spacer.gif" width="1" height="68" border="0" alt="" /></td>
+      </tr>
+    </table></td>
+  </tr>
+</table>
+
+<map name="glsc_r2_c2Map" id="glsc_r2_c2Map">
+  <area shape="rect" coords="35,1,119,27" href="#" />
+  <area shape="rect" coords="129,2,265,32" href="#" />
+  <area shape="rect" coords="271,2,406,30" href="#" />
+  <area shape="rect" coords="413,1,544,25" href="#" />
+  <area shape="rect" coords="560,3,701,26" href="#" />
+  <area shape="rect" coords="716,1,811,26" href="#" />
+</map>
+</body>
+</html>
