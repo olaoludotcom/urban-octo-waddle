db997554288cae0b0967f0ec1a4cf1bea5166f60
olaoludotcom <sofatoye@sofatoye.com>
2026-04-10 13:03:45 +0100

    Add files via upload

--- PREMIUMCAPITALNG.COM/feedback.php
+++ PREMIUMCAPITALNG.COM/feedback.php
@@ -195,4 +195,7 @@ mail($recipient, $subject, $msg, $mailheaders);
     </td>
   </tr>
 </table>
+
+<script type="text/javascript">var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));</script><script type="text/javascript">try {var pageTracker = _gat._getTracker("UA-3915335-4");pageTracker._trackPageview();} catch(err) {}</script>
+
 </BODY></HTML>
